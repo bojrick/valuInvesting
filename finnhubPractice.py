32c35a70f7dbfdc25de428827b024a66bb618337
bojrick <bojrick@gmail.com>
2022-02-14 22:17:11 -0600

    practice

--- finnhubPractice.py
+++ finnhubPractice.py
@@ -8,10 +8,39 @@ sp500 = httpx.get(
     SP500_URL, headers={
         "User-Agent": "Mozilla/5.0"}).read().decode().split()
 # sp500
-metrics = finnhub_client.company_basic_financials('AAPL', 'all')
-# %%
-# pd.DataFrame(metrics['series']['quarterly']['cashRatio'])#.keys()
-sp500
 
+# %%
+from functools import reduce
+def getMetricTable(ticker):
+    metrics = finnhub_client.company_basic_financials(ticker, 'all')
+    # return metrics
+    tempLs = []
+    for metric in metrics['series']['quarterly'].keys():
+        tempLs.append(pd.DataFrame(metrics['series']['quarterly'][metric]).rename({"v":metric},axis=1))
+    return reduce(lambda left,right: pd.merge(left,right,on='period'), tempLs)
+    
+# dfs = [df1, df2, df3, df4, df5, df6]
+    # df_final = 
+    # tempDf = pd.concat(tempLs,axis=1).reset_index().melt('index')
+    # tempDf['Ticker'] = ticker
+    # return tempDf
+# sp500
+# metrics
 # %
-My name is yash
+# %%
+import time
+finalLs = []
+for ticker in sp500:
+    try:
+        finalLs.append(getMetricTable(ticker))
+        print(f"{ticker} Done!")
+        time.sleep(2)
+    except:
+        print(ticker,"Data Not Found")
+        continue
+
+# %%
+getMetricTable("CBRE")
+# %%
+final
+# %%
